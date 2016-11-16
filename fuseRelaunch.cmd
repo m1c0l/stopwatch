1b2fd80e4d507492c8f96cf8e55bac280768960e
Michael Li <michael.li11702@gmail.com>
2016-11-15 19:31:00 -0800

    Delete rebased stuff

--- fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/michael/Documents/GitHub/stopwatch/master_tb_isim_beh.exe" -prj "C:/Users/michael/Documents/GitHub/stopwatch/master_tb_beh.prj" "work.master_tb" "work.glbl" 
